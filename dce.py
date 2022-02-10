e75ddf2449875e27f63ebae69603973a04d6ab1b
Niansong Zhang <zhangns@mail2.sysu.edu.cn>
2022-02-10 02:17:09 -0500

    update dce

--- dce.py
+++ dce.py
@@ -4,6 +4,7 @@ bril2json < *.bril | python dce.py | brili -p # profile dynamic instr count
 """
 import json
 import sys
+import argparse
 from copy import copy
 from basic_block import form_basic_blocks
 
@@ -11,7 +12,7 @@ from basic_block import form_basic_blocks
 # - every instruction is a dict
 # - you can delete dict in a list by list.remove(dict)
 
-def local_dce(block):
+def dce(block, global_dce=False):
     # Candidate instr to remove
     # These are instrs that assigns to a variable
     # that are not used yet
@@ -28,7 +29,7 @@ def local_dce(block):
                     candidates.pop(arg)
         # Check for def (assign)
         if 'dest' in instr: # if instr has destination
-            if instr['dest'] in candidates:
+            if instr['dest'] in candidates and not global_dce:
                 # the var is defined, and now redefining it
                 instr_to_rm = candidates[instr['dest']]
                 block.remove(instr_to_rm)
@@ -39,26 +40,37 @@ def local_dce(block):
         block.remove(instr)  
     return block  
 
-def iterate_to_converge(block):
+def iterate_to_converge(block, global_dce):
     iter_num = 0
     while True: 
         old_len = len(block)
-        new_block = local_dce(block)
+        new_block = dce(block, global_dce)
         new_len = len(new_block)
         iter_num += 1
         if old_len == new_len: break
         block = new_block
     return new_block
 
-def main():
+def main(global_dce=False):
     prog = json.load(sys.stdin)
-    for func in prog['functions']:
-        blocks = form_basic_blocks(func['instrs'])
-        new_blocks = list()
-        for block in blocks:
-            new_blocks.extend(iterate_to_converge(block))
-        func['instrs'] = new_blocks
+    if global_dce:
+        for func in prog['functions']:
+            func['instrs'] = iterate_to_converge(func['instrs'], global_dce)
+    else:
+        for func in prog['functions']:
+            blocks = form_basic_blocks(func['instrs'])
+            new_blocks = list()
+            for block in blocks:
+                new_blocks.extend(iterate_to_converge(block, global_dce))
+            func['instrs'] = new_blocks
     print(json.dumps(prog))
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', dest='global_dce',
+                        default=False, action='store_true',
+                        help='global dce')
+
+    args = parser.parse_args()
+    global_dce = args.global_dce
+    main(global_dce)
